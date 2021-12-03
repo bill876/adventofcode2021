bfb2a2a00ef9ab9c1526d3b3f41801213943605d
Vladimir Evsyukov <thebill876@gmail.com>
2021-12-03 19:07:34 +0600

    add solution to day 3 part 2

--- 03/index.ts
+++ 03/index.ts
@@ -8,20 +8,29 @@ function cleanInput(input: string): string[][] {
     .map(s => s.split(""))
 }
 
+function mostCommonBit(arr: string[]): "0"|"1" {
+  const zeroCount = arr
+    .filter(s => s === "0")
+    .length
+
+  const oneCount = arr.length - zeroCount
+
+  return zeroCount > oneCount ? "0" : "1"
+}
+
+function column(matrix: string[][], j: number): string[] {
+  return matrix
+    .map(s => s.slice(j, j+1)[0])
+}
+
 function part1(input: string[][]): number {
   const numberCount = input.length
   const numberLength = input[0].length
 
   let gammaBin = ""
   for (let j = 0; j < numberLength; j += 1) {
-    const zeroCount = input
-      .map(s => s.slice(j, j+1)[0])
-      .filter(s => s === "0")
-      .length
-
-    const oneCount = numberCount - zeroCount
-
-    const mostCommon = zeroCount > oneCount ? "0" : "1"
+    const jthColumn = column(input, j)
+    const mostCommon = mostCommonBit(jthColumn)
     gammaBin += mostCommon
   }
 
@@ -30,8 +39,36 @@ function part1(input: string[][]): number {
   return gammaRate * epsilonRate
 }
 
+function part2(input: string[][]): number {
+
+  const filterStep = (rows: string[][], colIndex: number, targetValue: string) => {
+    return rows.filter((row) => row.slice(colIndex, colIndex+1)[0] === targetValue)
+  }
+  let rows = [...input]
+  let j = 0
+  while (rows.length > 1) {
+    const mostCommon = mostCommonBit(column(rows, j))
+    rows = filterStep(rows, j, mostCommon)
+    j += 1
+  }
+  const o2RatingBinStr = rows[0].join("")
+  const o2Rating = parseInt(o2RatingBinStr, 2)
+
+  rows = [...input]
+  j = 0
+  while (rows.length > 1) {
+    const leastCommon = mostCommonBit(column(rows, j)) === "0" ? "1" : "0"
+    rows = filterStep(rows, j, leastCommon)
+    j += 1
+  }
+  const co2RatingBinStr = rows[0].join("")
+  const co2Rating = parseInt(co2RatingBinStr, 2)
+
+  return co2Rating * o2Rating
+}
+
 const cleanedInput = cleanInput(input)
 const part1Output = part1(cleanedInput)
 console.log("part 1:", part1Output)
-// const part2Output = part2(cleanedInput)
-// console.log("part 2:", part2Output)
+const part2Output = part2(cleanedInput)
+console.log("part 2:", part2Output)
